76d50e09374fe540a7b8f53694720b313308656b
sabrina <38015100+sabrinatitem@users.noreply.github.com>
2024-02-15 20:50:01 +0100

    Update print_colors.sh

--- src/print_colors.sh
+++ src/print_colors.sh
@@ -4,5 +4,5 @@
 source colors.sh
 
 # Print messages in different colors
-echo -e "${RED}This is some red text,${ENDCOLOR}"
+echo -e "${BRed}This is some red text,${ENDCOLOR}"
 echo -e "${GREEN}And this is some green text${ENDCOLOR}"
