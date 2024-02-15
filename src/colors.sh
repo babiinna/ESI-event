70696e9c2e8a78dac414b683b664b2900cdc5b3e
sabrina titem <sabrina.titem@emploitic.com>
2024-02-15 21:10:19 +0100

    fix: color not displaying normal on terminal

--- src/colors.sh
+++ src/colors.sh
@@ -1,80 +1,80 @@
 if [[ $TERM = "xterm-256color" ]] || [[ -n $COLORTERM ]]; then
     RED="\e[31m"
     GREEN="\e[32m"
-    # ENDCOLOR="\e[0m"
+    ENDCOLOR="\e[0m"
 else
     RED=""
     GREEN=""
-    # ENDCOLOR=""
+    ENDCOLOR=""
 fi
 
 # Regular Colors
 ENDCOLOR='\e[0m'
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Magenta='\e[0;35m'      # Magenta
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
+BLACK='\e[0;30m'        # Black
+RED='\e[0;31m'          # Red
+GREEN='\e[0;32m'        # Green
+YELLOW='\e[0;33m'       # Yellow
+BLUE='\e[0;34m'         # Blue
+MAGENTA='\e[0;35m'      # Magenta
+CYAN='\e[0;36m'         # Cyan
+WHITE='\e[0;37m'        # White
 
 # Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BMagenta='\e[1;35m'     # Magenta
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
+BBLACK='\e[1;30m'       # Black
+BRED='\e[1;31m'         # Red
+BGREEN='\e[1;32m'       # Green
+BYELLOW='\e[1;33m'      # Yellow
+BBLUE='\e[1;34m'        # Blue
+BMAGENTA='\e[1;35m'     # Magenta
+BCYAN='\e[1;36m'        # Cyan
+BWHITE='\e[1;37m'       # White
 
 # Underline
-UBlack='\e[4;30m'       # Black
-URed='\e[4;31m'         # Red
-UGreen='\e[4;32m'       # Green
-UYellow='\e[4;33m'      # Yellow
-UBlue='\e[4;34m'        # Blue
-UMagenta='\e[4;35m'     # Magenta
-UCyan='\e[4;36m'        # Cyan
-UWhite='\e[4;37m'       # White
+UBLACK='\e[4;30m'       # Black
+URED='\e[4;31m'         # Red
+UGREEN='\e[4;32m'       # Green
+UYELLOW='\e[4;33m'      # Yellow
+UBLUE='\e[4;34m'        # Blue
+UMAGENTA='\e[4;35m'     # Magenta
+UCYAN='\e[4;36m'        # Cyan
+UWHITE='\e[4;37m'       # White
 
 # Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Magenta='\e[45m'     # Magenta
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
+ON_BLACK='\e[40m'       # Black
+ON_RED='\e[41m'         # Red
+ON_GREEN='\e[42m'       # Green
+ON_YELLOW='\e[43m'      # Yellow
+ON_BLUE='\e[44m'        # Blue
+ON_MAGENTA='\e[45m'     # Magenta
+ON_CYAN='\e[46m'        # Cyan
+ON_WHITE='\e[47m'       # White
 
 # High Intensity
-IBlack='\e[0;90m'       # Black
-IRed='\e[0;91m'         # Red
-IGreen='\e[0;92m'       # Green
-IYellow='\e[0;93m'      # Yellow
-IBlue='\e[0;94m'        # Blue
-IMagenta='\e[0;95m'     # Magenta
-ICyan='\e[0;96m'        # Cyan
-IWhite='\e[0;97m'       # White
+IBLACK='\e[0;90m'       # Black
+IRED='\e[0;91m'         # Red
+IGREEN='\e[0;92m'       # Green
+IYELLOW='\e[0;93m'      # Yellow
+IBLUE='\e[0;94m'        # Blue
+IMAGENTA='\e[0;95m'     # Magenta
+ICYAN='\e[0;96m'        # Cyan
+IWHITE='\e[0;97m'       # White
 
 # Bold High Intensity
-BIBlack='\e[1;90m'      # Black
-BIRed='\e[1;91m'        # Red
-BIGreen='\e[1;92m'      # Green
-BIYellow='\e[1;93m'     # Yellow
-BIBlue='\e[1;94m'       # Blue
-BIMagenta='\e[1;95m'    # Magenta
-BICyan='\e[1;96m'       # Cyan
-BIWhite='\e[1;97m'      # White
+BIBLACK='\e[1;90m'      # Black
+BIRED='\e[1;91m'        # Red
+BIGREEN='\e[1;92m'      # Green
+BIYELLOW='\e[1;93m'     # Yellow
+BIBLUE='\e[1;94m'       # Blue
+BIMAGENTA='\e[1;95m'    # Magenta
+BICYAN='\e[1;96m'       # Cyan
+BIWHITE='\e[1;97m'      # White
 
 # High Intensity backgrounds
-On_IBlack='\e[0;100m'   # Black
-On_IRed='\e[0;101m'     # Red
-On_IGreen='\e[0;102m'   # Green
-On_IYellow='\e[0;103m'  # Yellow
-On_IBlue='\e[0;104m'    # Blue
-On_IMagenta='\e[0;105m' # Magenta
-On_ICyan='\e[0;106m'    # Cyan
-On_IWhite='\e[0;107m'   # White
+ON_IBLACK='\e[0;100m'   # Black
+ON_IRED='\e[0;101m'     # Red
+ON_IGREEN='\e[0;102m'   # Green
+ON_IYELLOW='\e[0;103m'  # Yellow
+ON_IBLUE='\e[0;104m'    # Blue
+ON_IMAGENTA='\e[0;105m' # Magenta
+ON_ICYAN='\e[0;106m'    # Cyan
+ON_IWHITE='\e[0;107m'   # White
