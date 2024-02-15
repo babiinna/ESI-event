0a45b31c9f135c17c006e4d9d44a4139085de612
sabrina titem <sabrina.titem@emploitic.com>
2024-02-15 21:10:19 +0100

    update colors.sh

--- src/colors.sh
+++ src/colors.sh
@@ -1,12 +1,12 @@
-if [[ $TERM = "xterm-256color" ]] || [[ -n $COLORTERM ]]; then
-    RED="\e[31m"
-    GREEN="\e[32m"
-    # ENDCOLOR="\e[0m"
-else
-    RED=""
-    GREEN=""
-    # ENDCOLOR=""
-fi
+# if [[ $TERM = "xterm-256color" ]] || [[ -n $COLORTERM ]]; then
+#     RED="\e[31m"
+#     GREEN="\e[32m"
+#     # ENDCOLOR="\e[0m"
+# else
+#     RED=""
+#     GREEN=""
+#     # ENDCOLOR=""
+# fi
 
 # Regular Colors
 ENDCOLOR='\e[0m'
