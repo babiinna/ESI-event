62502fd3cb9215543bb90e6eb147d515e6ff0182
sabrina <38015100+sabrinatitem@users.noreply.github.com>
2024-02-15 20:28:49 +0100

    Update colors.sh

--- src/colors.sh
+++ src/colors.sh
@@ -1,3 +1,13 @@
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
+if [[ $TERM = "xterm-256color" ]] || [[ -n $COLORTERM ]]; then
+    RED="\e[31m"
+    GREEN="\e[32m"
+    ENDCOLOR="\e[0m"
+else
+    RED=""
+    GREEN=""
+    ENDCOLOR=""
+fi
+
+# RED="\e[31m"
+# GREEN="\e[32m"
+# ENDCOLOR="\e[0m"
