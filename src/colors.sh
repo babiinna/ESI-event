655e5efe9de94e6ad664e1feb40b6b02261a97d0
sabrina <38015100+sabrinatitem@users.noreply.github.com>
2024-02-16 16:39:59 +0100

    fix: color not displaying normal on terminal (#7)
    Co-authored-by: sabrina titem <sabrina.titem@emploitic.com>

--- src/colors.sh
+++ src/colors.sh
@@ -1,6 +1,23 @@
+if [[ $TERM = "xterm-256color" ]] || [[ -n $COLORTERM ]]; then
+    RED="\e[31m"
+    GREEN="\e[32m"
+    ENDCOLOR="\e[0m"
+else
+    RED=""
+    GREEN=""
+    ENDCOLOR=""
+fi
 
 # Regular Colors
 ENDCOLOR='\e[0m'
+BLACK='\e[0;30m'        # Black
+RED='\e[0;31m'          # Red
+GREEN='\e[0;32m'        # Green
+YELLOW='\e[0;33m'       # Yellow
+BLUE='\e[0;34m'         # Blue
+MAGENTA='\e[0;35m'      # Magenta
+CYAN='\e[0;36m'         # Cyan
+WHITE='\e[0;37m'        # White
 BLACK='\e[0;30m'        # Black
 RED='\e[0;31m'          # Red
 GREEN='\e[0;32m'        # Green
@@ -19,8 +36,24 @@ BBLUE='\e[1;34m'        # Blue
 BMAGENTA='\e[1;35m'     # Magenta
 BCYAN='\e[1;36m'        # Cyan
 BWHITE='\e[1;37m'       # White
+BBLACK='\e[1;30m'       # Black
+BRED='\e[1;31m'         # Red
+BGREEN='\e[1;32m'       # Green
+BYELLOW='\e[1;33m'      # Yellow
+BBLUE='\e[1;34m'        # Blue
+BMAGENTA='\e[1;35m'     # Magenta
+BCYAN='\e[1;36m'        # Cyan
+BWHITE='\e[1;37m'       # White
 
 # Underline
+UBLACK='\e[4;30m'       # Black
+URED='\e[4;31m'         # Red
+UGREEN='\e[4;32m'       # Green
+UYELLOW='\e[4;33m'      # Yellow
+UBLUE='\e[4;34m'        # Blue
+UMAGENTA='\e[4;35m'     # Magenta
+UCYAN='\e[4;36m'        # Cyan
+UWHITE='\e[4;37m'       # White
 UBLACK='\e[4;30m'       # Black
 URED='\e[4;31m'         # Red
 UGREEN='\e[4;32m'       # Green
@@ -39,8 +72,24 @@ ON_BLUE='\e[44m'        # Blue
 ON_MAGENTA='\e[45m'     # Magenta
 ON_CYAN='\e[46m'        # Cyan
 ON_WHITE='\e[47m'       # White
+ON_BLACK='\e[40m'       # Black
+ON_RED='\e[41m'         # Red
+ON_GREEN='\e[42m'       # Green
+ON_YELLOW='\e[43m'      # Yellow
+ON_BLUE='\e[44m'        # Blue
+ON_MAGENTA='\e[45m'     # Magenta
+ON_CYAN='\e[46m'        # Cyan
+ON_WHITE='\e[47m'       # White
 
 # High Intensity
+IBLACK='\e[0;90m'       # Black
+IRED='\e[0;91m'         # Red
+IGREEN='\e[0;92m'       # Green
+IYELLOW='\e[0;93m'      # Yellow
+IBLUE='\e[0;94m'        # Blue
+IMAGENTA='\e[0;95m'     # Magenta
+ICYAN='\e[0;96m'        # Cyan
+IWHITE='\e[0;97m'       # White
 IBLACK='\e[0;90m'       # Black
 IRED='\e[0;91m'         # Red
 IGREEN='\e[0;92m'       # Green
@@ -59,8 +108,24 @@ BIBLUE='\e[1;94m'       # Blue
 BIMAGENTA='\e[1;95m'    # Magenta
 BICYAN='\e[1;96m'       # Cyan
 BIWHITE='\e[1;97m'      # White
+BIBLACK='\e[1;90m'      # Black
+BIRED='\e[1;91m'        # Red
+BIGREEN='\e[1;92m'      # Green
+BIYELLOW='\e[1;93m'     # Yellow
+BIBLUE='\e[1;94m'       # Blue
+BIMAGENTA='\e[1;95m'    # Magenta
+BICYAN='\e[1;96m'       # Cyan
+BIWHITE='\e[1;97m'      # White
 
 # High Intensity backgrounds
+ON_IBLACK='\e[0;100m'   # Black
+ON_IRED='\e[0;101m'     # Red
+ON_IGREEN='\e[0;102m'   # Green
+ON_IYELLOW='\e[0;103m'  # Yellow
+ON_IBLUE='\e[0;104m'    # Blue
+ON_IMAGENTA='\e[0;105m' # Magenta
+ON_ICYAN='\e[0;106m'    # Cyan
+ON_IWHITE='\e[0;107m'   # White
 ON_IBLACK='\e[0;100m'   # Black
 ON_IRED='\e[0;101m'     # Red
 ON_IGREEN='\e[0;102m'   # Green
--- src/print_colors.sh
+++ src/print_colors.sh
@@ -4,5 +4,11 @@
 source colors.sh
 
 # Print messages in different colors
-echo -e "${BRed}This is some red text,${ENDCOLOR}"
-echo -e "${GREEN}And this is some green text${ENDCOLOR}"
+# echo -e "${BRed}This is some red text,${ENDCOLOR}"
+# echo -e "${GREEN}And this is some green text${ENDCOLOR}"
+for i in {0..255} ; do
+    printf "\e[38;5;%sm  %3s  \e[0m" $i $i
+    if [ $((($i + 1) % 16)) == 0 ] ; then
+        echo # New line every 16 colors
+    fi
+done
